9350cdcd75d0498981d81ebbc82d3d14c5b69d10
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-31 10:38:05 -0500

    bug

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require("../../models");
+
 
 
 // get all users
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
-        order: [['created_at', 'DESC']],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -26,7 +42,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -61,6 +83,16 @@ router.post('/', (req, res) => {
         });
 });
 
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
 router.put('/:id', (req, res) => {
     Post.update(
         {
@@ -84,6 +116,7 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
